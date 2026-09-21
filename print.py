1a15e23cdf0a2acd92e20d45dc9509a40279dad9
Karan Dhami <Karandhami2054@gmail.com>
2026-09-21 14:29:08 +0545

    edit \r

--- print.py
+++ print.py
@@ -1,12 +1,12 @@
-# import time
-# def usingFunctions(): 
-#     count =0 
-#     while count<10: 
-#         print("\rmrcet cse dept ",count, end="") 
-#         time.sleep(1)
-#         count=count+1 
+import time
+def usingFunctions(): 
+    count =0 
+    while count<10: 
+        print("\rmrcet cse dept ",count, end="") 
+        time.sleep(1)
+        count=count+1 
  
-# usingFunctions() 
+usingFunctions() 
 
 for i in range(10):
     print("Hello\r world\r", i)
