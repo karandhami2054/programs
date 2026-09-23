2ad946165658b469c85807352553ef7bdb8eeea7
Karan Dhami <Karandhami2054@gmail.com>
2026-09-23 14:59:01 +0545

    sort dictionary key in ascending and values in descending order

---
+++ dict_1.py
@@ -0,0 +1,12 @@
+dict = {
+    'karan':10,
+    'dhami': 5,
+    'hari' : 15,
+    'pratik' : 8
+}
+
+
+keys = sorted(dict.keys())
+values = sorted(dict.values(), reverse=True)
+print("Keys:", keys)
+print("Values:", values)
